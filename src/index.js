d862199071c051d66d7db3a58ab2deff40ad3c24
Nikola Stefanovski <nikoding32@gmail.com>
2023-02-16 17:38:22 +0100

    atatus added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('2b13a83a6fcc4920913038be3b73686e').install();
 
 const root = document.getElementById('root');
   render(
